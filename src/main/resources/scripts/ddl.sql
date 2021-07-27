6f4299df6612d4ad5afc9aa26188fd6783c979aa
Rafael Augusto <rafaelpontes13@hotmail.com>
2021-07-26 23:23:25 -0300

    ajuste no script e criação do .env

--- src/main/resources/scripts/ddl.sql
+++ src/main/resources/scripts/ddl.sql
@@ -4,3 +4,20 @@ CREATE TABLE IF NOT EXISTS video (
    url VARCHAR(2000) NOT NULL
 );
 
+
+CREATE TABLE IF NOT EXISTS category (
+   id serial PRIMARY KEY,
+   title VARCHAR (2000) NOT NULL,
+   color VARCHAR(2000) NOT NULL
+);
+
+ALTER TABLE video
+ADD COLUMN category_id INT;
+
+ALTER TABLE video
+ADD CONSTRAINT fk_category
+FOREIGN KEY (category_id)
+REFERENCES category (id);
+
+INSERT INTO category (id, title, color) VALUES (1, 'LIVRE', 'black');
+
